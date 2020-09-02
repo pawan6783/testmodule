80c97d178601d1af10b17c2b044e48300e1f885e
Pawan Saroj <pawansaroj6783@gmail.com>
2020-09-02 10:22:50 +0530

    test cases added

--- index.js
+++ index.js
@@ -1,11 +1,57 @@
-//reverse the string
-exports.revert = function(str){
 
-    if(str==" ")
-    return "empty string";
+const functions = {
     
-    return str.split("").reverse().join("");
+    revert: function(str){
 
+        if(str==" ")
+        return "empty string";
+        
+        return str.split("").reverse().join("");
+    
+    },
+
+    bubblesort: function(list){
+            
+            let i;
+            let j;
+            let stop;
+    
+       
+    
+            try{
+                if(typeof list == null){
+                return null;
+            }
+    
+            let len = list.length;
+            
+            if(len==0){
+                return "Empty list";
+            }
+    
+            for (i=0; i<len; i++)
+            {
+                for(j=0, stop=len-i; j<stop; j++)
+                {
+                    if(list[j] >list[j+1]){
+                        functions.swap(list,j,j+1);
+                    }
+                }
+            }
+        }
+    
+        catch(err){
+            console.log(err);
+        }
+       
+        return list;
+    },
+
+    swap: (arr, first , second) => {
+        let temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
 }
 
-//console.log(this.revert("hello"));
+module.exports = functions;
